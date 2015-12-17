f19a5140257749461845e1f8bb403a072f1b08b3
liuyuan <843073660@qq.com>
2015-12-17 11:03:41 +0800

    2015.12.17

---
+++ Demo/src/liuyuan210512/SwitchCaseDemo.java
@@ -0,0 +1,23 @@
+package liuyuan210512;
+
+public class SwitchCaseDemo {
+
+	public static void main(String[] args) {
+		 int  a = 2;
+		switch (a) {
+		case 1:
+			System.out.println("1");
+			break;
+		case 3:
+			System.out.println("3");
+			break;
+		case 4:
+			System.out.println("4");
+		default:
+			System.out.println("there is no ");
+			break;
+		}
+
+	}
+
+}
